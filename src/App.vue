a66d8b50f62de2d1e20869de896bdbf1a4e534fd
nothingct <nothingct@naver.com>
2022-11-05 11:40:26 +0900

    vue3-component-first

---
+++ src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <MyBtn
+    large
+    color="red">
+    Just Go
+  </MyBtn>
+  <MyBtn
+    color="black">
+    <span style="color : red">I'll Go</span>
+  </MyBtn>
+  <MyBtn
+    :color="color">
+    For YOUITOI
+  </MyBtn>
+  <MyBtn
+    large>
+    Just I'll Go
+  </MyBtn>
+</template>
+
+<script>
+import MyBtn from '~/components/Mybtn';
+
+export default {
+  components: {
+    MyBtn
+  },
+  data(){
+    return{
+      color : '#000'
+    }
+  }
+}
+</script>
+
